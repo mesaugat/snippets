467b20734db6a7187280a7d1cca27359c8975745
Saugat Acharya <mesaugat@gmail.com>
2019-09-11 21:51:27 +0545

    Allow script to pass arguments

--- aws/aws-iam-user-policies.sh
+++ aws/aws-iam-user-policies.sh
@@ -3,6 +3,7 @@
 set -e
 
 ###
+#
 # List of IAM users and their attached policies in AWS for a quick audit.
 # Note: Doesn't include inline policies.
 #
@@ -10,6 +11,7 @@ set -e
 #
 # Usage:
 #   ./aws-iam-user-policies.sh
+#   ./aws-iam-user-policies.sh --profile something
 #
 # Output:
 #   AWS IAM Users and Policies
@@ -18,9 +20,10 @@ set -e
 #   | etl_dev                                  | etl_dev_s3_policy
 #   | etl_prod                                 | etl_prod_s3_policy
 #   | somebody_that_i_used_to_know             | IAMUserChangePassword - (g) AdministratorAccess
+#
 ###
 
-users=$(aws iam list-users | jq -r ".Users[].UserName")
+users=$(aws "$@" iam list-users | jq -r ".Users[].UserName")
 
 if [ -z "${users}" ]; then
   printf "Your AWS account has no users. Woah! ¯\_(ツ)_/¯"
@@ -32,12 +35,12 @@ printf "AWS IAM Users and Policies\n\n"
 printf "| %-40s | %s \n" "Username" "Policies"
 
 for user in $users; do
-  policies=$(aws iam list-attached-user-policies --user-name "$user" | jq -r '.AttachedPolicies | map(.PolicyName) | join(", ")')
-  user_groups=$(aws iam list-groups-for-user --user-name "$user" | jq -r '.Groups[].GroupName')
+  policies=$(aws "$@" iam list-attached-user-policies --user-name "$user" | jq -r '.AttachedPolicies | map(.PolicyName) | join(", ")')
+  user_groups=$(aws "$@" iam list-groups-for-user --user-name "$user" | jq -r '.Groups[].GroupName')
   group_policies=
 
   for group in $user_groups; do
-    group_policies+=$(aws iam list-attached-group-policies --group-name "$group" | jq -r '.AttachedPolicies | map(.PolicyName) | join(", ")')
+    group_policies+=$(aws "$@" iam list-attached-group-policies --group-name "$group" | jq -r '.AttachedPolicies | map(.PolicyName) | join(", ")')
 
     if [ -n "$group_policies" ]; then
       group_policies+=", "
